68bfb8486076231dffa748ba64898a86b96f94d0
Jian Tay <jian.tay@colorado.edu>
2025-02-20 13:53:33 -0700

    Updated plots

--- test_plotdata.m
+++ test_plotdata.m
@@ -12,16 +12,34 @@ totalDiffuse = totalProtein;
 %Make a bar plot showing total protein concentration in this image
 for iCell = 1:numel(celldata)
 
-    totalProtein(iCell) = celldata(iCell).TotalIntDiffuse + celldata(iCell).TotalIntSpot;
+    totalProtein(iCell) = (celldata(iCell).TotalIntDiffuse + celldata(iCell).TotalIntSpot)/...
+        numel(celldata(iCell).PixelIdxList);
+    
+    totalDiffuse(iCell) = celldata(iCell).TotalIntDiffuse/numel(celldata(iCell).DiffusePixelIdxList);
+
     totalSpot(iCell) = celldata(iCell).TotalIntSpot;
-    totalDiffuse(iCell) = celldata(iCell).TotalIntDiffuse;
+    spotVol = 0;
+    for ii = 1:numel(celldata(iCell).SpotPixelIdxList)
+
+        spotVol = spotVol + numel(celldata(iCell).SpotPixelIdxList{ii});
+
+    end
+
+    totalSpotConc(iCell) = totalSpot(iCell) / spotVol;
 
 end
 
 %%
-stackedData = cat(2, totalDiffuse, totalSpot);
+figure;
 
-bar(stackedData, 'stacked')
-ylabel('Total intensity')
-xlabel('Cell')
+scatter(totalProtein, totalDiffuse)
+hold on
+scatter(totalProtein, totalSpotConc)
+
+% 
+% stackedData = cat(2, totalDiffuse, totalSpot);
+% 
+% bar(stackedData, 'stacked')
+% ylabel('Total intensity')
+% xlabel('Cell')
 legend('Diffuse', 'Punctate')
