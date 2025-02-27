9e818ac86d7f0a28d4c4d4fd5ce6c95d63200134
Jian Tay <jian.tay@colorado.edu>
2025-02-27 09:17:55 -0700

    WIP: Commit before changing spot detection

--- test_segmentation.m
+++ test_segmentation.m
@@ -1,7 +1,8 @@
 clearvars
 clc
 
-file = 'D:\Work\ALMC\houghlab-yeast-quantification\data\yJM1837_Sup35MstartWT_richmedia002.nd2';
+%file = 'D:\Work\ALMC\houghlab-yeast-quantification\data\yJM1837_Sup35MstartWT_richmedia002.nd2';
+file = '../data/yJM1837_Sup35WT_4_5hrs_EDmedia021.nd2';
 
 reader = BioformatsImage(file);
 
@@ -37,16 +38,78 @@ dd = imhmin(dd, 1);
 LL = watershed(dd);
 
 mask(LL == 0) = 0;
+
+mask = imerode(mask, strel('sphere', 3));
+
 showoverlay(imadjust(imageData(:, :, 16)), bwperim(mask(:, :, 16)));
 
 %% Find vacuoles
+%meanCellInt = mean(imageData(mask), 'all');
 
-cellData = regionprops3(mask, imageData, 'MeanIntensity', 'VoxelIdxList', 'VoxelValues');
+%vacMask = imageDataSmoothed <= (0.9 * meanCellInt);
+
+vacMask = false(size(mask));
+spotMask = false(size(mask));
+
+cellData = regionprops3(mask, imageDataSmoothed, 'MeanIntensity', 'VoxelIdxList', 'VoxelValues');
 
 for iCell = 1:height(cellData)
-    
-    
+
+    %Calculate the vacuole threshold
+    vacTh = 1.2 * cellData(iCell, :).MeanIntensity;
+
+    %Find voxels which are below this threshold
+    isVacuole = cellData(iCell, :).VoxelValues{:} < vacTh;
+
+    %Update the mask
+    idxList = cellData(iCell, :).VoxelIdxList{:};
+    idxList = idxList(isVacuole);
+    vacMask(idxList) = true;
+
+    %Recalculate the mean cell intensity for spot detection
+    vxVal = cellData(iCell, :).VoxelValues{:};
+    netMeanIntensity = mean(vxVal(~isVacuole));
+    spotTh = 4 * cellData(iCell, :).MeanIntensity; %Should modify to exclude vacuoles?
+
+    %Find voxels which are below this threshold
+    isSpot = cellData(iCell, :).VoxelValues{:} > spotTh;
+
+    %Update the mask
+    idxList = cellData(iCell, :).VoxelIdxList{:};
+    idxList = idxList(isSpot);
+    spotMask(idxList) = true;    
+
+
 end
+%showoverlay(imadjust(imageData(:, :, 16)), vacMask(:, :, 16));
+%%
+plane = 18;
+showoverlay(imageData(:, :, plane), bwperim(spotMask(:, :, plane)))
+
+%%
+cellMask = bwlabeln(mask);
+cellMask(vacMask) = 0;
+
+outputDir = '../processed/20250227';
+outputFN = 'tmp';
+
+%Renormalize the image data for output image
+imageData = double(imageData);
+imageData = (imageData - min(imageData, [], 'all')) / (max(imageData, [], 'all') - min(imageData, [], 'all'));
+
+for iZ = 1:size(imageData, 3)
+
+    imgOut = showoverlay(imageData(:, :, iZ), bwperim(cellMask(:, :, iZ)), 'Color', [0 1 0]);
+    imgOut = showoverlay(imgOut, bwperim(spotMask(:, :, iZ)), 'Color', [1 0 1]);
+
+    if iZ == 1
+        imwrite(imgOut, fullfile(outputDir, [outputFN, '.tiff']), 'Compression', 'none')
+    else
+        imwrite(imgOut, fullfile(outputDir, [outputFN, '.tiff']), 'Compression', 'none', 'WriteMode', 'append')
+    end
+
+end
+
 
 
 %imshow(imadjust(currPlane), [])
